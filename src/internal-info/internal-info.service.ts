c55a02469807eba609d76e1efe624f887dbf8326
NitichaiAom <NitichaiSawangsai@gmail.com>
2022-02-09 15:36:29 +0700

    feat: create internal

--- src/internal-info/internal-info.service.ts
+++ src/internal-info/internal-info.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import moment from 'moment';
+import { Brackets, Repository } from 'typeorm';
 import { InternalInfo } from './internal-info.entity';
+import { InternalInfoStatusType } from './internal-info.enum';
 
 @Injectable()
 export class InternalInfoService {
@@ -11,7 +13,44 @@ export class InternalInfoService {
   ) {}
 
   autoInactive() {
-    console.log('aaaa <><><><><><><>< \n\n\n\n\n');
-    return 122;
+    const queryBuilder = this.internalInfoRepository
+      .createQueryBuilder('internal')
+      .where('internal.changeInactive = :changeInactive', {
+        changeInactive: false,
+      })
+      .andWhere('internal.status = :status', {
+        status: InternalInfoStatusType.Active,
+      })
+      .andWhere(
+        new Brackets((qb) => {
+          qb.orWhere(
+            new Brackets((qb) => {
+              qb.where('internal.expireDate IS NULL');
+              qb.andWhere('internal.createdAt > :date', {
+                date: moment().add(1, 'M').toDate(),
+              });
+            }),
+          );
+          qb.orWhere(
+            new Brackets((qb) => {
+              qb.where('internal.expireDate IS NOT NULL');
+              qb.andWhere('internal.expireDate < :date', {
+                date: moment().toDate(),
+              });
+            }),
+          );
+        }),
+      );
+
+    return queryBuilder
+      .select([
+        'internal.id',
+        'internal.title',
+        'internal.expireDate',
+        'internal.changeInactive',
+        'internal.status',
+        'internal.createdAt',
+      ])
+      .getMany();
   }
 }
