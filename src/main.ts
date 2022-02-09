6796cfe84f9f64717058f91dece4e52adc4152c5
NitichaiAom <NitichaiSawangsai@gmail.com>
2022-02-09 14:31:00 +0700

    feat: create internal

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { LOGGER_SERVICE } from './app.constants';
 import { AppModule } from './app.module';
@@ -5,6 +6,8 @@ import { WinstonLoggerService } from './logger/winston-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.useLogger(app.get<WinstonLoggerService>(LOGGER_SERVICE));
 
--- src/serverless.ts
+++ src/serverless.ts
@@ -1,12 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+import { WinstonLoggerService } from './logger/winston-logger.service';
+import { LOGGER_SERVICE } from './app.constants';
 
 let server: Handler;
 
-async function bootstrap(): Promise<Handler> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useLogger(app.get<WinstonLoggerService>(LOGGER_SERVICE));
+
+  await app.listen(3000, '0.0.0.0');
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
@@ -18,6 +27,9 @@ export const handler: Handler = async (
   context: Context,
   callback: Callback,
 ) => {
+  console.log('event ', event);
+  console.log('context ', context);
+  console.log('callback ', callback);
   server = server ?? (await bootstrap());
   return server(event, context, callback);
 };
